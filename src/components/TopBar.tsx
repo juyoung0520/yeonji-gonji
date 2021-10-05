86516457315144f136e2dda72545c7ca91f081a0
jeonchaei <ahswn1031@gmail.com>
2021-10-05 15:13:38 +0900

    dd

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,27 @@
+import styled from '@emotion/styled'
 import React from 'react'
 
+const Header = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 63px;
+  background-color: white;
+  padding: 17px 16px 16px;
+`
+const HeartButton = styled.button`
+  margin: 0;
+  background-color: red;
+`
 function TopBar() {
-  return <div></div>
+  return (
+    <div>
+      <Header>
+        <HeartButton />
+      </Header>
+    </div>
+  )
 }
 
 export default TopBar
