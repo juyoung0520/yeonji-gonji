9f2e589358dfb2f2a2df9144656e6b8d7a903d78
JJY <juyoung0520@naver.com>
2021-10-05 14:47:59 +0900

    ㄹ

---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled'
+import React, { ReactNode } from 'react'
+
+const Container = styled.div`
+  padding: 3rem 0;
+  background-color: White;
+  min-height: 100vh;
+  height: 100%;
+`
+
+const Content = styled.div`
+  max-width: 1268.8px;
+  margin: 0 auto;
+  background-color: White;
+  padding: 1.5rem;
+`
+interface ComponentProps {
+  children: ReactNode
+}
+
+function Layout({ children }: ComponentProps) {
+  return (
+    <Container>
+      <Content>{children}</Content>
+    </Container>
+  )
+}
+
+export default Layout
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import Layout from '../components/Layout'
+import MainContents from '../components/MainContents'
+import TopBar from '../components/TopBar'
+
 function Main() {
-  return <div></div>
+  return (
+    <Layout>
+      <TopBar />
+      <MainContents />
+    </Layout>
+  )
 }
 
 export default Main
