663f91ecea9110e9f13508a3c6fdabbfe9b6c488
jeonchaei <ahswn1031@gmail.com>
2021-10-05 14:49:06 +0900

    d

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,12 +5,16 @@ import MainContents from '../components/MainContents'
 import TopBar from '../components/TopBar'
 
 function Main() {
+<<<<<<< Updated upstream
   return (
     <Layout>
       <TopBar />
       <MainContents />
     </Layout>
   )
+=======
+  return <div> </div>
+>>>>>>> Stashed changes
 }
 
 export default Main
