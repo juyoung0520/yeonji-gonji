4a9c74aa5b679cf9ad54de57bd5dca21018abd9a
jeonchaei <ahswn1031@gmail.com>
2021-10-05 14:12:19 +0900

    Auto stash before merge of "dev" and "origin/dev"

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+<<<<<<< Updated upstream
 function Footer() {
   return <div></div>
 }
 
 export default Footer
+=======
+function Main() {
+  return <div></div>
+}
+
+export default Main
+>>>>>>> Stashed changes
